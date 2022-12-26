abccc733e9bc05c9faf45fcc894e899ebdb50469
eichtici-sys <henryjesus.htc@gmail.com>
2022-12-25 23:53:45 -0500

    blog finished

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,11 +1,48 @@
+import { getPosts } from "~/models/posts.server"
+import { useLoaderData } from "@remix-run/react"
+import Post from "~/components/post"
+import styles from '~/styles/blog.css'
 
+export function meta(){
+  return {
+    title: 'GuitarLA - Nuestro Blog',
+    description: 'GuitarLA, Blog de Musica y Venta de Guitarras'
+  }
+}
+
+export function links(){
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+
+export async function loader(){
+  const posts = await getPosts()  
+  return posts.data
+}
 
 function Blog() {
+
+  const posts = useLoaderData()
+  
   return (
-    <div>
-      Desde Blog
-      
-    </div>
+    <main className="contenedor">
+      <h2 className="heading">Blog</h2>
+      <div className="blog">
+        {
+          posts.map(post => (
+            <Post
+              key={post.id}
+              post = {post.attributes}
+            ></Post>
+          ))
+        }
+      </div>
+    </main>
   )
 }
 
